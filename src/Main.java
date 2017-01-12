ba26ac85ac20449dd0cc58c80385489e427ca098
DESKTOP-JSD7FNH\ryugwanghyen <ryudung@zuminternet.com>
2017-01-12 11:22:11 +0900

    7.오브젝트 팩토리의 활용(제어의역전)

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
 import dao.UserDao;
 import domain.User;
 import factory.DaoFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
 
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-      UserDao userDao = new DaoFactory().getUserDao();
-      User user=userDao.get("ryudung");
+
+/*        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        UserDao userDao = context.getBean("UserDao", UserDao.class);*/
+        UserDao userDao = new DaoFactory().UserDao();
+        User user = userDao.get("ryudung");
         System.out.println(user.toString());
     }
 }
--- src/factory/DaoFactory.java
+++ src/factory/DaoFactory.java
@@ -1,31 +1,37 @@
 package factory;
 
 import connection.ConnectionMaker;
+import connection.NConnectionMaker;
 import connection.ZConnectionMaker;
 import dao.UserDao;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * *
  * * Created by ryudung on 2017-01-12.
  * *
- *      UserDao객체를 생성해주는 팩토리 클래스
+ *      UserDao객체를 생성해주는 팩토리 클래스(IoC)
+ *      기존에 클라이언트에서 오브젝트를 생성해주었다.
+ *      생성해주는 책임을 분리하여, 다른 오브젝트 팩토리에서 생성하여 클라이언트에서 넘겨 받아 클라이언트는 사용만한다.
+ *      IoC를 함으로써 생성의 책임(관심사)을 분리할 수 있게 되었다.
  **/
-public class DaoFactory {
-    public UserDao getUserDao() {
-        ConnectionMaker connectionMaker = new ZConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+//@Configuration
+public class DaoFactory { //이 역할을 스프링이 해주게 된다.
+    //@Bean
+    public UserDao UserDao() {
+
+        UserDao userDao = new UserDao(connectionMaker());
         return userDao;
     }
+    //@Bean
+    public UserDao NUserDao() {
 
-/*    public UserDao getZUserDao() {  //문제점 : 생성해야할 종류가 많아 질수록 코드가 늘어난다.
-        ConnectionMaker connectionMaker = new ZConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
+        UserDao userDao = new UserDao(connectionMaker());
         return userDao;
     }
-
-    public UserDao getNUserDao() {
-        ConnectionMaker connectionMaker = new ZConnectionMaker();
-        UserDao userDao = new UserDao(connectionMaker);
-        return userDao;
-    }*/
+   // @Bean
+    public ConnectionMaker connectionMaker(){
+        return new ZConnectionMaker();
+    }
 }
