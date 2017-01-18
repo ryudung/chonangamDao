6462a304743f9b275910cbc941eab8b04a2432ba
DESKTOP-JSD7FNH\ryugwanghyen <ryudung@zuminternet.com>
2017-01-18 11:29:06 +0900

    XML설정 및 DL

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import domain.User;
 import factory.DaoFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
@@ -10,14 +11,13 @@ import java.sql.SQLException;
 public class Main {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
-        UserDao userDao = context.getBean("UserDao", UserDao.class);
-        System.out.println(userDao.change(1));
+        ApplicationContext context = new GenericXmlApplicationContext("config.xml");
+        UserDao userDao = context.getBean("userDao", UserDao.class);
+
        /* UserDao userDao = new DaoFactory().UserDao();*/
         User user = userDao.get("ryudung");
         System.out.println(user.toString());
 
-        UserDao userDao2 = context.getBean("UserDao", UserDao.class);
-        System.out.println(userDao.change(1));
+      //  UserDao userDao2 = context.getBean("UserDao", UserDao.class);
     }
 }
